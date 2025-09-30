a3acdb321042135d29838002400b4dca05b27c89
Harshit Kasera <harshitkasera01@gmail.com>
2025-10-01 00:02:38 +0530

    api

--- Api.js
+++ Api.js
@@ -1,7 +1,7 @@
 const Questions = [
     {
   "question": "What is 12 + 8?",
-  "options":["18", "5", "20", "22"],
+  "options":["20", "5", "21", "22"],
   "start": "harshit",
   "correctAnswer": 0,
   "subject": "Aptitude",
@@ -9,7 +9,7 @@ const Questions = [
 },
  {
   "question": "What is 12 - 6?",
-  "options":["5", "6", "18", "22"],
+  "options":["6", "7", "18", "22"],
   "start": "harshit",
   "correctAnswer": 0,
   "subject": "Aptitude",
@@ -17,7 +17,7 @@ const Questions = [
 },
 {
   "question": "What is -10 - 6?",
-  "options":["6", "-19", "-16", "22"],
+  "options":["6", "-16", "16", "22"],
   "start": "harshit",
   "correctAnswer": 1,
   "subject": "Aptitude",
@@ -25,14 +25,14 @@ const Questions = [
 },
 {
   "question": "What is 10 * 6?",
-  "options":["64", "60", "62", "22"],
+  "options":["60", "62", "68", "22"],
   "start": "harshit",
   "correctAnswer": 0,
   "subject": "Aptitude",
   "difficulty": "easy"
 },{
   "question": "What is 12 / 6?",
-  "options":["6", "3", "2", "2.5"],
+  "options":["8", "6", "2", "2.5"],
   "start": "harshit",
   "correctAnswer": 1,
   "subject": "Aptitude",
